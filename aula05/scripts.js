608cfe15d812f65499f61fe0c39e44a42b99949f
MaercioMamedes <maerciomamedes@hotmail.com>
2021-09-19 17:21:37 -0300

    atualização aula05

--- aula05/scripts.js
+++ aula05/scripts.js
@@ -1,8 +1,32 @@
 function adicionarFilme(){
+    var contador = document.querySelectorAll("figure").length;
     var tituloFilme = document.querySelector("input#tituloFilme");
     var urlFilme = document.querySelector("input#urlFilme");
-    var filme = [tituloFilme.value,urlFilme.value];
+    var filme = [tituloFilme.value,urlFilme.value,contador];
     tituloFilme.value ="";
     urlFilme.value = "";
+
+    listarFilmes(filme)
+}
+function listarFilmes(filme){
+    var galeria = document.querySelector("div#listaFilmes");
+    var figure = document.createElement("figure");
+    var legenda = document.createElement("figcaption");
+    legenda.innerHTML="<h3>"+filme[0]+"</h3>";
+    var image = document.createElement("img");
+    var button = document.createElement("button");
+    button.innerText="REMOVER";
+    button.addEventListener("click",apagar);
+    image.src=filme[1];
+    image.id="figura"+filme[2];
+    figure.id=filme[2];
+    figure.appendChild(image);
+    figure.appendChild(legenda);
+    figure.appendChild(button);
+    galeria.appendChild(figure);
+
 }
 
+function apagar(){
+    console.log("olá mundo");
+}
