7b73dfc308ee8fe42e23b17dc5c558bfde5d1aa3
MaercioMamedes <maerciomamedes@hotmail.com>
2021-09-19 22:17:22 -0300

    atualização aula 05 final

--- aula05/scripts.js
+++ aula05/scripts.js
@@ -1,5 +1,5 @@
 function adicionarFilme(){
-    var contador = document.querySelectorAll("figure").length;
+    var contador = document.querySelectorAll("figure");
     var tituloFilme = document.querySelector("input#tituloFilme");
     var urlFilme = document.querySelector("input#urlFilme");
     var filme = [tituloFilme.value,urlFilme.value,contador];
@@ -8,7 +8,11 @@ function adicionarFilme(){
 
     listarFilmes(filme)
 }
+
 function listarFilmes(filme){
+
+    figuras = filme[2];
+    contadorID = parseInt(figuras.length);
     var galeria = document.querySelector("div#listaFilmes");
     var figure = document.createElement("figure");
     var legenda = document.createElement("figcaption");
@@ -16,10 +20,31 @@ function listarFilmes(filme){
     var image = document.createElement("img");
     var button = document.createElement("button");
     button.innerText="REMOVER";
-    button.addEventListener("click",apagar);
     image.src=filme[1];
-    image.id="figura"+filme[2];
-    figure.id=filme[2];
+
+
+    /* Controle de ID's. Para evitar que mais de uma tag figure, tenham ID's repetidos
+     Sempre a primeira imagem terá índice zero, a partir dessas sempre, será o índice da última mais um
+     mesmo que alguma figura de índice menor seja excluída   */
+
+
+    if(figuras.length>0){
+
+        var ultimaFigura = figuras[figuras.length-1];
+        var proximoID = parseInt(ultimaFigura.id)+1;
+        button.id="btn"+ proximoID;
+        image.id="figura"+proximoID;
+        figure.id=proximoID;
+        
+    } else{
+    
+        button.id="btn"+contadorID;
+        image.id="figura"+contadorID;
+        figure.id=contadorID;
+
+    }
+
+    button.addEventListener("click",apagar,button); // atribuição de método para exclusão de figura
     figure.appendChild(image);
     figure.appendChild(legenda);
     figure.appendChild(button);
@@ -27,6 +52,8 @@ function listarFilmes(filme){
 
 }
 
-function apagar(){
-    console.log("olá mundo");
+function apagar(a){
+    button = a.path[1];  // esse método, extrai vários dados sobre o obejto recebido pelo parâmetro, neste caso, o ID do botão,
+    var galeria = document.querySelector("div#listaFilmes");
+    galeria.removeChild(button);
 }
